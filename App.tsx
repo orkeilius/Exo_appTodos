ee9b909b5ddc8702f0be6972e0e60763678d6003
orkeilius <orkeilius@users.noreply.github.com>
2025-02-01 15:31:16 +0100

    add paper

--- App.tsx
+++ App.tsx
@@ -2,12 +2,15 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import {NavigationContainer} from "@react-navigation/native";
 import TodoScreen from "./Screen/TodoScreen";
+import {PaperProvider} from "react-native-paper";
 
 export default function App() {
   return (
       <NavigationContainer>
-        <StatusBar style="auto" />
+        <PaperProvider>
+          <StatusBar style="auto" />
           <TodoScreen />
+        </PaperProvider>
       </NavigationContainer>
   )
 }
